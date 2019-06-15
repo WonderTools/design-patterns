e17c0b4688ef0c182eecbcfe15082254a3477dc4
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-06-15 14:08:42 +0530

    Added the seat belt feature

--- MyCar/MyCar/Bootstrap.cs
+++ MyCar/MyCar/Bootstrap.cs
@@ -9,30 +9,30 @@ namespace WonderTools.MyCar
             Console.WriteLine("Please press the option");
             Console.WriteLine("i/I for increase car speed");
             Console.WriteLine("d/D for decrease car speed");
-            Console.WriteLine("e/E for decrease car speed");
-
+            Console.WriteLine("u/U for unlock car");
+            Console.WriteLine("l/L for lock car");
+            Console.WriteLine("e/E for exit the application");
 
             var simulator = new CarSpeedSimulator();
             var speedometer = new Speedometer(simulator);
 
-
             var alarm = new Alarm();
             var speedAlarm = new SpeedAlarm(alarm, speedometer);
-            
+            var seatBelt = new SeatBelt(alarm, speedometer);
+
             while (true)
             {
                 var key = Console.ReadKey();
                 Console.WriteLine();
                 var keyChar = key.KeyChar;
-                if ((keyChar == 'i') || (keyChar == 'I'))
-                {
-                    simulator.Increase();
-                }
-                else if ((keyChar == 'd') || (keyChar == 'D'))
-                {
-                    simulator.Decrease();
-                }
-                else if ((keyChar == 'e') || (keyChar == 'E')) break;
+                var optionString = keyChar.ToString().ToLower();
+
+                if (optionString == "i") simulator.Increase();
+                else if (optionString == "d") simulator.Decrease();
+                else if (optionString == "l") seatBelt.Lock();
+                else if (optionString == "u") seatBelt.UnLock();
+                else if (optionString == "e") break;
+                else Console.WriteLine("unknown option");
             }
         }
     }
--- MyCar/MyCar/SpeedAlarm.cs
+++ MyCar/MyCar/SpeedAlarm.cs
@@ -20,4 +20,37 @@
             _oldSpeed = speed;
         }
     }
+
+    public class SeatBelt : ISpeedObserver
+    {
+
+        private readonly Alarm _alarm;
+        private int _oldSpeed = 0;
+        private bool _isLocked = false;
+
+        public SeatBelt(Alarm alarm, Speedometer speedometer)
+        {
+            _alarm = alarm;
+            speedometer.AddSpeedObserver(this);
+        }
+
+        public void Lock()
+        {
+            _isLocked = true;
+        }
+
+        public void UnLock()
+        {
+            _isLocked = false;
+        }
+
+        public void OnSpeedChange(int speed)
+        {
+            if ((_oldSpeed < 20) && (speed >= 20))
+            {
+                _alarm.RaiseAlarm();
+            }
+            _oldSpeed = speed;
+        }
+    }
 }
