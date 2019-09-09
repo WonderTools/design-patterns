cc388349a365fee6dedf02a14f093cd44d6765ef
Nachiappan <nachiappan.nk.ext@sivantos.com>
2019-09-09 22:44:37 +0530

    Solution for problem 1

---
+++ Problem01/Solution/MyCar/MyCar/SpeedAlarm.cs
@@ -0,0 +1,26 @@
+﻿namespace MyCar
+{
+    public class SpeedAlarm : ISpeedObserver
+    {
+        private readonly Alarm _alarm;
+        private int _oldSpeed = 0;
+
+        public SpeedAlarm(Alarm alarm)
+        {
+            _alarm = alarm;
+        }
+        private void ProcessSpeed(int speed)
+        {
+            if ((_oldSpeed < 80) && (speed >= 80))
+            {
+                _alarm.RaiseAlarm();
+            }
+            _oldSpeed = speed;
+        }
+
+        public void OnSpeedChange(int speed)
+        {
+            ProcessSpeed(speed);
+        }
+    }
+}
---
+++ Problem01/Solution/MyCar/MyCar/Speedometer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyCar
+{
+    public class Speedometer
+    {
+        private readonly SpeedAlarm _alarm;
+
+        List<ISpeedObserver> _observers = new List<ISpeedObserver>();
+
+        public void RegisterObserver(ISpeedObserver speedObserver)
+        {
+            _observers.Add(speedObserver);
+        }
+
+        public Speedometer(CarSpeedSimulator speedSimulator, SpeedAlarm alarm)
+        {
+            _alarm = alarm;
+            speedSimulator.SpeedChanged += OnSpeedChanged;
+        }
+
+        private void OnSpeedChanged(int speed)
+        {
+            Console.WriteLine("Current Speed :" + speed);
+            foreach (var speedObserver in _observers)
+            {
+                speedObserver.OnSpeedChange(speed);
+            }
+        }
+    }
+}
